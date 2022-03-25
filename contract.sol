9baf368b03218ce07341d5cb759e3c2bdb4efe75
RegEdit <47848969+ghRegEdit@users.noreply.github.com>
2022-03-25 22:10:11 +0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.6;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 
-contract SBER-LP is Ownable {
+contract SBER_LP is Ownable {
 
     uint256 private number;
     uint256 public amount;
